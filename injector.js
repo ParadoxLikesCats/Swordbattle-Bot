9b1012c1f0d3427e44bc5f16b3743eefa6f67eaa
ParadoxLikesCats <97798276+ParadoxLikesCats@users.noreply.github.com>
2022-01-20 10:36:47 -0500

    Create injector.js

---
+++ injector.js
@@ -0,0 +1,32 @@
+const validPages = {
+    "swordbattle.codergautamyt.repl.co": {
+        scriptURL: "https://hackhost.sedated.repl.co/script.js"
+    }
+};
+
+function injectScript(url) {
+    let script = document.createElement('script');
+    script.src = url;
+    document.getElementsByTagName('head')[0].appendChild(script);
+}
+
+function hookCheat(url) {
+    injectScript("https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js");
+    const waitJquery = () => {
+        if (window.jQuery) {
+            injectScript(validPages[url].scriptURL);
+        } else {
+            setTimeout(() => waitJquery(), 50);
+        }
+    }
+    waitJquery();
+}
+
+function getPage() {
+    let url = window.location.hostname;
+    if (url in validPages) {
+        hookCheat(url);
+    }
+}
+
+getPage();
